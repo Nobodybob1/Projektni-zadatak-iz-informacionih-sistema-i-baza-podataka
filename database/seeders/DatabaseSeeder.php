91aee1e844c76fec20d2ba6e6ae433158ca2c063
deskimir <desimirdimovic@gmail.com>
2023-02-13 16:45:21 +0100

    genrator optimizacija jos nesto mal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,7 +30,7 @@ class DatabaseSeeder extends Seeder
         for($i=0; $i<$total_offers; $i+=$batch_size){
             $offers = \App\Models\Offer::factory($batch_size)->make()->toArray();
             for($j=0; $j<$batch_size; $j++){
-                if($i+$j < 834){ // 166 * 60 = 9960 neaktivnih ponuda
+                if($i+$j < 50000){ 
                     $offers[$j]['is_active'] = "1";  //aktivni
                 }else{
                     $offers[$j]['is_active'] = "0";  //neaktivni
@@ -49,20 +49,12 @@ class DatabaseSeeder extends Seeder
             ->pluck('id')
             ->toArray();
             $offer->accommodations()->attach($accommodation_ids);
-
-                // $accommodation_ids = \App\Models\Accommodation::inRandomOrder()->pluck('id')->toArray();
-                // $num_cities = count(explode(',', $offer->days)) - 1;
-                // shuffle($accommodation_ids);
-                // for($i=0;$i<$num_cities;$i++){
-                //     $offer->accommodations()->attach($accommodation_ids[$i]);
-                // }
         }
 
         
 
         \App\Models\User::create(["name"=> "admin","username"=>"admin","password"=>bcrypt("admin"),"is_admin"=>"1"]);
         \App\Models\User::create(["name"=> "done","username"=>"done","password"=>bcrypt("done"),"is_admin"=>"1"]);
-        // $this->call(NotActiveOffersSeeder::class);
         
     }
 
